131b65ec44e8335b4f3808b006d8d4052093e701
[van quynh] <[quynhnvph29577@fpt.edu.vn]>
2023-11-18 19:54:50 +0700

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 8080;
+export const mongoDBURL =
+    'mongodb+srv://quynhnvph29577:KfG83ZkHEAdWzHVk@cluster0.j3o4usa.mongodb.net/BookStose-MERN-stack?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routers/booksRoute.js"
+import cors from 'cors'
 const app = express();
+
+//Middleware for parsing request body
+app.use(express.json())
+
+
+app.use(cors())
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
+
 
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send(`Wellcome To MERT Stack Tutorial`)
 })
-
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-})
+app.use('/books', booksRoute)
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database")
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
